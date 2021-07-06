6fc68934d168f19d97095f737263def839995a43
kuwabara <takashi.k@cayto.jp>
2021-07-07 00:48:02 +0900

    テスト追加

--- internal/db/repository/sushiDataMock.go
+++ internal/db/repository/sushiDataMock.go
@@ -10,34 +10,31 @@ type MockSushiData struct {
 }
 
 func (m MockSushiData) First(i interface{}, _ ...interface{}) *gorm.DB {
-	i = model.SushiData{
-		ID:    99,
-		Name:  "mock",
-		Price: 100,
-		Sozai: `["納豆","のり","しゃり"]`,
-	}
+	p := i.(*model.SushiData)
+	p.ID = 99
+	p.Name = "mock"
+	p.Price = 99
+	p.Sozai = `["納豆","のり","しゃり"]`
 	return new(gorm.DB)
 }
 
 func (m MockSushiData) Create(i interface{}) *gorm.DB {
-	i = model.SushiData{
-		ID:    99,
-		Name:  "mock",
-		Price: 100,
-		Sozai: "mock",
-	}
+	p := i.(*model.SushiData)
+	p.ID = 99
+	p.Name = "mock"
+	p.Price = 99
+	p.Sozai = `["納豆","のり","しゃり"]`
 	return new(gorm.DB)
 }
 
 func (m MockSushiData) Finds(_ string, _ int, i interface{}) error {
-	i = []model.SushiData{
-		{
-			ID:    99,
-			Name:  "mock",
-			Price: 100,
-			Sozai: "mock",
-		},
-	}
+	p := i.(*[]model.SushiData)
+	*p = append(*p, model.SushiData{
+		ID:    99,
+		Name:  "mock",
+		Price: 99,
+		Sozai: `["納豆","のり","しゃり"]`,
+	})
 	return nil
 }
 
--- internal/usecase/sushi_test.go
+++ internal/usecase/sushi_test.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"reflect"
 	"sushiApi/internal/db/repository"
+	"sushiApi/internal/http/gen"
 	"testing"
 )
 
@@ -29,6 +30,167 @@ func TestNewSushi(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := NewSushi(tt.args.db); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("NewSushi() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSushi_FindSushiById(t *testing.T) {
+	type fields struct {
+		sd repository.SushiDataInterface
+	}
+	type args struct {
+		id int64
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *gen.Sushi
+		wantErr bool
+	}{
+		{
+			name: "OK",
+			fields: fields{
+				sd: repository.NewMockSushiData(),
+			},
+			args: args{
+				id: 1,
+			},
+			want: &gen.Sushi{
+				NewSushi: gen.NewSushi{
+					Name:  "mock",
+					Price: 99,
+					Sozai: []string{"納豆", "のり", "しゃり"},
+				},
+				Id: 1,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Sushi{
+				sd: tt.fields.sd,
+			}
+			got, err := p.FindSushiById(tt.args.id)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindSushiById() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FindSushiById() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSushi_AddSushi(t *testing.T) {
+	type fields struct {
+		sd repository.SushiDataInterface
+	}
+	type args struct {
+		param *gen.NewSushi
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *gen.Sushi
+		wantErr bool
+	}{
+		{
+			name: "OK",
+			fields: fields{
+				sd: repository.NewMockSushiData(),
+			},
+			args: args{
+				param: &gen.NewSushi{
+					Name:  "mock",
+					Price: 99,
+					Sozai: []string{"納豆", "のり", "しゃり"},
+				},
+			},
+			want: &gen.Sushi{
+				NewSushi: gen.NewSushi{
+					Name:  "mock",
+					Price: 99,
+					Sozai: []string{"納豆", "のり", "しゃり"},
+				},
+				Id: 99,
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Sushi{
+				sd: tt.fields.sd,
+			}
+			got, err := p.AddSushi(tt.args.param)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("AddSushi() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("AddSushi() got = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSushi_FindSushis(t *testing.T) {
+	type fields struct {
+		sd repository.SushiDataInterface
+	}
+	type args struct {
+		params gen.FindSushisParams
+	}
+	isAsc := false
+	limit := int32(99)
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		want    *[]gen.Sushi
+		wantErr bool
+	}{
+		{
+			name: "OK",
+			fields: fields{
+				sd: repository.NewMockSushiData(),
+			},
+			args: args{
+				params: gen.FindSushisParams{
+					Asc:   &isAsc,
+					Limit: &limit,
+				},
+			},
+			want: &[]gen.Sushi{
+				{
+					NewSushi: gen.NewSushi{
+						Name:  "mock",
+						Price: 99,
+						Sozai: []string{"納豆", "のり", "しゃり"},
+					},
+					Id: 99,
+				},
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			p := &Sushi{
+				sd: tt.fields.sd,
+			}
+			got, err := p.FindSushis(tt.args.params)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("FindSushis() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("FindSushis() got = %v, want %v", got, tt.want)
 			}
 		})
 	}
